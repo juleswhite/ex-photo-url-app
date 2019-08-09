d7a4a4665850f5f2f5370bacbd867f3665849069
Jules White <jules.white@gmail.com>
2019-08-09 07:42:25 -0500

    Added instructions

---
+++ README.md
@@ -0,0 +1,5 @@
+# Instructions
+
+Clone and import this project into Eclipse as an Existing Gradle Project.
+
+See the edu.vanderbilt.finsta.PhotoController class for instructions.
--- src/main/java/edu/vanderbilt/finsta/PhotoController.java
+++ src/main/java/edu/vanderbilt/finsta/PhotoController.java
@@ -22,45 +22,98 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class PhotoController {
 
-	private Map<String, List<PhotoUrl>> photosByTag = new HashMap<>();
 	
-	private void addPhoto(PhotoUrl photo, String tag) {
-		List<PhotoUrl> photos = photosByTag.getOrDefault(tag, new ArrayList<PhotoUrl>());
-		photos.add(photo);
-		photosByTag.put(tag, photos);
-	}
-	
-	@RequestMapping("/photo")
-	public boolean addPhoto(String url, String tag) {
-		PhotoUrl photo = new PhotoUrl();
-		photo.setUrl(url);
-		photo.setTime(System.currentTimeMillis());
-		addPhoto(photo,tag);
-		return true;
-	}
-	
-	@RequestMapping("/photo/{tag}")
-	public List<PhotoUrl> getPhotosByTag(@PathVariable String tag){
-		return photosByTag.getOrDefault(tag, new ArrayList<>());
-	}
-	
-	@RequestMapping("/tag")
-	public Set<String> getTags() {
-		return photosByTag.keySet();
-	}
-	
-	@RequestMapping("/photo/search")
-	public List<PhotoUrl> searchPhotosByTagPrefix(String tagPrefix){
-		List<String> matchingTags = photosByTag
-				.keySet().stream()
-				.filter(t -> t.startsWith(tagPrefix))
-				.collect(Collectors.toList());
-		
-		return matchingTags.stream()
-				.flatMap(t -> photosByTag.get(t).stream())
-				.collect(Collectors.toList());
-	}
+	/**
+        
+        Step 1. 
 
+        Create a method call addPhoto that receives requests to /photo
+        and has url and tag arguments. The method should create a new
+        PhotoUrl, set the Url and time for the PhotoUrl, and then add
+        the PhotoUrl to a list photos associated with the specified
+        tag. Finally, the method should return true if the operation
+        was successful.
+
+        When you are done, you should be able to open the link below
+        in your browser and see "true" printed:
+
+        http://localhost:8080/photo?url=http://www.magnum.io/img/magnum.png&tag=vandy
+
+        **/
+
+	
+	/**
+        
+        Step 2. 
+
+        Create a method called getPhotosByTag that receives requests to
+        /photo/{tag}, where {tag} is replaced by an arbitrary string.
+        The method should take a tag argument. The method should return
+        the list of photos that have been associated with the specified
+        tag.
+
+        When you are done, you should be able to open the link below
+        in your browser and see something similar to what is shown
+        below the link.
+
+        http://localhost:8080/photo/vandy
+
+        [{"id":"766da867-ca7c-4dc0-89fe-7528fd1cddd0",
+          "time":1565353765112,
+          "url":"http://www.magnum.io/img/magnum.png"}]
+
+        **/
+
+	/**
+        
+        Step 3. 
+
+        Create a method called getTags() that receives requests to /tag
+        and takes no arguments. The method should return the current 
+        list of tags in use. 
+
+         When you are done, you should be able to open the link below
+        in your browser and see something similar to what is shown
+        below the link.    
+        
+        http://localhost:8080/tag
+
+        ["vandy"]
+
+        **/	
+
+	/**
+        
+        Step 5. 
+
+        Create a method called searchPhotosByTagPrefix that receives
+        requests to /photo/search and takes a tagPrefix argument. The
+        method should return the list of all photos where the tag for
+        the photo starts with the provided prefix. 
+
+        **/
+
+
+        /**
+    
+        Step 6. 
+
+        This example uses a Thymeleaf template to render an HTML response.
+
+        Open the src/main/resources/templates/stream.html file and modify
+        this Thymeleaf template (look near the bottom) to also print out
+        the time associated with each PhotoUrl.     
+
+        You should be able to go to http://localhost:8080/photo/vandy/stream
+        to see the photo stream html.
+
+        Try to use the examples that already exist in the code. If you need
+        additional help, you can refer to the Thymeleaf template docs:
+        https://www.thymeleaf.org/doc/tutorials/3.0/usingthymeleaf.html#introducing-thymeleaf
+
+        This code is highly insecure.
+
+        **/
 	@RequestMapping("/photo/{tag}/stream")
 	public ModelAndView photoStream(@PathVariable String tag) {
 		ModelAndView mv = new ModelAndView("stream");
@@ -69,8 +122,19 @@ public class PhotoController {
 		return mv;
 	}
 	
-	 @RequestMapping(value="/photo/search/stream", produces=MediaType.TEXT_HTML_VALUE)
-     public String boom(String tagPrefix) throws URISyntaxException {     
+        /**
+    
+        Step 7. 
+
+        This example uses raw string manipulation to render an HTML response.
+        
+        Modify the code to wrap each image in a "<div></div>" element and also
+        include the time that the photo was taken inside the div.
+        
+        This code is highly insecure.
+        **/
+	@RequestMapping(value="/photo/search/stream", produces=MediaType.TEXT_HTML_VALUE)
+        public String boom(String tagPrefix) throws URISyntaxException {     
 		 List<PhotoUrl> photos = searchPhotosByTagPrefix(tagPrefix);
          
 		 String result = "<html><body>";
@@ -82,11 +146,23 @@ public class PhotoController {
 		 result += "</body></html>";
 		 
 		 return result;
-     }
+        }
 	 
-	 @RequestMapping(value="/profile", produces=MediaType.IMAGE_JPEG_VALUE)
-	 public byte[] getProfilePic(String user) throws IOException {
+        /**
+    
+        Step 8. 
+
+        This example shows how to read a binary file and return it to a client.
+
+        Try to create your own method with a different name that
+        serves up files from some directory using the correct content type.
+
+        This code is highly insecure.
+        
+        **/
+	@RequestMapping(value="/profile", produces=MediaType.IMAGE_JPEG_VALUE)
+	public byte[] getProfilePic(String user) throws IOException {
 		 return Files.readAllBytes(Paths.get("src/main/resources/static/"+user));
-	 }
+	}
 	
 }
