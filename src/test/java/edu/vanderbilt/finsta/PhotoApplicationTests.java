68ca63eb9069567135b91df3b57afc6a064b9fcb
QUCHEN FU <31392274+QuchenFu@users.noreply.github.com>
2020-07-19 22:33:31 -0500

    Update PhotoApplicationTests.java
    add test

--- src/test/java/edu/vanderbilt/finsta/PhotoApplicationTests.java
+++ src/test/java/edu/vanderbilt/finsta/PhotoApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.util.Assert;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PhotoController.class)
@@ -15,12 +18,66 @@ public class PhotoApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	private ObjectMapper objectMapper = new ObjectMapper();
 
-	@Test
-    public void testPostingAndListing() throws Exception {
-    	
-    	
-	}
+    @Test
+    public void testPhoto() throws Exception {
+        String result = mockMvc.perform(get("/photo?url=http://www.magnum.io/img/magnum.png&tag=vandy"))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        Assert.isTrue(result.equals("true"));
+    }
+
+    @Test
+    public void testGetPhotoByTag() throws Exception {
+        String result = mockMvc.perform(get("/photo/vandy"))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        Assert.isTrue(result.endsWith("\"url\":\"http://www.magnum.io/img/magnum.png\"}]"));
+    }
+
+    @Test
+    public void testGetTags() throws Exception {
+        String result = mockMvc.perform(get("/tag"))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        Assert.isTrue(result.endsWith("[\"vandy\"]"));
+    }
+
+    @Test
+    public void testSearchPhotosByTagPrefix() throws Exception {
+        String result = mockMvc.perform(get("/photo/search?tagPrefix=vandy"))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        Assert.isTrue(result.endsWith("\"url\":\"http://www.magnum.io/img/magnum.png\"}]"));
+    }
+
+//    @Test
+//    public void testBoom() throws Exception {
+//        String result = mockMvc.perform(get("/photo/search/stream"))
+//            .andExpect(status().isOk())
+//            .andReturn().getResponse().getContentAsString();
+//        System.out.println("#####################################");
+//        System.out.println(result);
+//        Assert.isTrue(result.endsWith());
+//    }
+
+    @Test
+    public void testPhotoStream() throws Exception {
+        String result = mockMvc.perform(get("/photo/vandy/stream?tag=vandy"))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        Assert.isTrue(result.contains("<img src=\"http://www.magnum.io/img/magnum.png\"></img>"));
+    }
+
+//    @Test
+//    public void testGetProfilePic() throws Exception {
+//        String result = mockMvc.perform(get("/profile"))
+//            .andExpect(status().isOk())
+//            .andReturn().getResponse().getContentAsString();
+//        System.out.println("#####################################");
+//        System.out.println(result);
+//        Assert.isTrue(result.contains());
+//    }
 }
